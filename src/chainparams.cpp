dd0eec9af3fc999d93e6ab85dd29f72764ad6283
Sawano <aldianokto@gmail.com>
2026-07-07 20:48:02 +0700

    Clean up comments in chainparams.cpp
    Removed comments about minimum chain work and default signature validity.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -108,10 +108,7 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1479168000; // November 15th, 2016.
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 0; // Disabled
 
-        // The best chain should have at least this much work.
         consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000000000000000200020");
-
-        // By default assume that the signatures in ancestors of this block are valid.
         consensus.defaultAssumeValid = uint256S("0x00000937cdbf9936fb1338bdb7ae61798c6d761d1d436606adef9a8bce33b6b4");
 
         // AuxPoW parameters
